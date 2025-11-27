22345919ebb0613bcbbb6aad73406741c1bebaef
darknight007 <darknight007@users.noreply.github.com>
2025-11-27 20:20:00 +0800

    setup

---
+++ pipeline/compile.py
@@ -0,0 +1,12 @@
+from kfp.v2 import compiler
+from pipeline import customer_spend_pipeline
+
+if __name__ == "__main__":
+    try:
+        compiler.Compiler().compile(
+            pipeline_func=customer_spend_pipeline,
+            package_path="customer_pipeline.json"
+    )
+    print("Compilation successful!")
+except Exception as e:
+    print(f"Compilation failed: {e}")
